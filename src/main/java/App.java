8c8979460a6b89ae9df222ac4653ba71c07d7425
Dmitry Zoteev <122165438+DmitriyZoteev@users.noreply.github.com>
2023-02-21 21:40:17 +0500

    PP_2_1_2_Spring_HelloWorld
    Task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorld1.getMessage());
+        System.out.println(helloWorld2.getMessage());
+        System.out.println(helloWorld1 == helloWorld2);
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+        System.out.println(cat2.getName());
+        System.out.println(cat1 == cat2);
     }
 }
